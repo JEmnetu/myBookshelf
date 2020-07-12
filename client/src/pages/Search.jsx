05f595885daa68dd122877f12f31486c82914dba
JEmnetu <j.habtema@gmail.com>
2020-07-12 04:32:47 -0400

    updated search function to update search result as input is updated.

--- client/src/pages/Search.jsx
+++ client/src/pages/Search.jsx
@@ -10,33 +10,45 @@ class Search extends Component {
     isLoaded: false,
   };
 
-  onSubmitSearch = (e) => {
-    e.preventDefault();
+  // onSubmitSearch = (e) => {
+  //   e.preventDefault();
 
-    const query = e.target[0].value;
+  //   const query = e.target[0].value;
+  //   if (query.length === 0) {
+  //     this.setState({ isLoaded: false });
+  //     return;
+  //   }
+  //   axios
+  //     .get(`https://www.googleapis.com/books/v1/volumes?q=${e.target[0].value}`)
+  //     .then((books) => {
+  //       this.setState({ books: books.data.items, isLoaded: true });
+  //     });
+  // };
+  onInputChange = (e) => {
+    const query = e.target.value;
     if (query.length === 0) {
       this.setState({ isLoaded: false });
       return;
     }
+
     axios
-      .get(`https://www.googleapis.com/books/v1/volumes?q=${e.target[0].value}`)
+      .get(`https://www.googleapis.com/books/v1/volumes?q=${e.target.value}`)
       .then((books) => {
         this.setState({ books: books.data.items, isLoaded: true });
       });
   };
-  // onInputChange = (e) => {
 
-  // };
-
-  // onSaveBook = (book) => {
-  //   console.log(book);
-  // };
   render() {
     return (
       <Container>
         <div id="search-flex">
           <form className="srd" onSubmit={this.onSubmitSearch}>
-            <input type="text" id="search" name="search" />
+            <input
+              onChange={this.onInputChange}
+              type="text"
+              id="search"
+              name="search"
+            />
             <button type="submit">Search</button>
           </form>
         </div>
