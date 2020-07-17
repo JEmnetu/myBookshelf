3cc34a516e1020a6dcb5ad60b29117f93a5803b3
JEmnetu <j.habtema@gmail.com>
2020-07-17 05:49:20 -0400

    make it better

--- server.js
+++ server.js
@@ -1,25 +1,14 @@
 const dotenv = require("dotenv").config();
 const express = require("express");
 const app = express();
-const config = require("config");
 const mongoose = require("mongoose");
+const path = require("path");
 
 const PORT = process.env.PORT || 6001;
 
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
-if (process.env.NODE_ENV === "production") {
-  // Exprees will serve up production assets
-  app.use(express.static("public"));
-
-  // Express serve up index.html file if it doesn't recognize route
-  const path = require("path");
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
-}
 
 // DB Config
 const db = process.env.MY_DB || process.env.MONGODB_URI;
@@ -32,5 +21,14 @@ mongoose
 
 // Import Routes
 app.use("/api/books", require("./routes/api/books"));
+
+//server static assets
+if (process.env.NODE_ENV === "production") {
+  //Set static folder
+  app.use(express.static("public/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "public", "build", "index.html"));
+  });
+}
 
 app.listen(PORT, () => console.log(`App is running on port ${PORT}`));
