991c1bcd94f655efc1e54c8e8b5f41a7563c99db
JEmnetu <j.habtema@gmail.com>
2020-07-17 06:17:09 -0400

    updating deployment scripts

--- server.js
+++ server.js
@@ -25,9 +25,9 @@ app.use("/api/books", require("./routes/api/books"));
 //server static assets
 if (process.env.NODE_ENV === "production") {
   //Set static folder
-  app.use(express.static("public/build"));
+  app.use(express.static("client/build"));
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "public", "build", "index.html"));
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
 
